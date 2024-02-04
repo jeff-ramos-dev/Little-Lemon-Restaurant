d1f799395868d19585dad7b948bdd8e0ca8841de
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2024-02-04 11:30:06 -0800

    Add djoser routes to project-level urls

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -9,5 +9,7 @@ router.register(r'tables', views.BookingViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('restaurant.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
     path('restaurant/booking/', include(router.urls))
 ]
