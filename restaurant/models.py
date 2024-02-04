f4e92800297e4a7ebd0945f81654c5a4856e5686
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2024-02-04 10:16:47 -0800

    Rename Menu model to MenuItem

--- restaurant/models.py
+++ restaurant/models.py
@@ -7,7 +7,7 @@ class Booking(models.Model):
     BookingDate = models.DateTimeField()
 
 
-class Menu(models.Model):
+class MenuItem(models.Model):
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.SmallIntegerField()
