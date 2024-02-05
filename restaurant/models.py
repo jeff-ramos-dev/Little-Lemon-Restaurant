149ed39af95714f8033da9bb3e1c594a75f245f7
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2024-02-04 21:34:26 -0800

    Move test file and define __str__ for MenuItem model

--- restaurant/models.py
+++ restaurant/models.py
@@ -11,4 +11,7 @@ class MenuItem(models.Model):
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.SmallIntegerField()
+
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
     
