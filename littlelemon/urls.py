23df937514058a15da3c059e389a612c2b44cb0e
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2024-02-06 12:15:08 -0800

    Restructure project urls

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -8,8 +8,8 @@ router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('restaurant.urls')),
+    path('', include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('restaurant/booking/', include(router.urls))
+    path('booking/', include(router.urls))
 ]
