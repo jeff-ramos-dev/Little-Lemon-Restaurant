9d06d0eb28cf6e7d4f94ab0445b5a2af3277f985
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2024-02-06 12:16:18 -0800

    Update app urls and views

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path 
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('menu/', views.MenuItemsView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('api-token-auth/', obtain_auth_token)
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
+    path('book/', views.book, name="book"),
+    path('bookings/', views.bookings, name="bookings"),
+    path('reservations/', views.reservations, name="reservations"),
+    path('api/token-auth/', obtain_auth_token),
+    path('api/menu/', views.MenuItemsView.as_view(), name='menu-api'),
+    path('api/menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,25 +1,114 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 from rest_framework import generics, viewsets 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, BasePermission, SAFE_METHODS
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer, BookingSerializer
+from .forms import BookingForm
+import datetime
+import json
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_superuser
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    current_year = str(datetime.date.today()).split('-')[0]
+    return render(request, 'index.html', {"current_year": current_year})
+
+def about(request):
+    current_year = str(datetime.date.today()).split('-')[0]
+    return render(request, 'about.html', {"current_year": current_year})
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdmin]
+        else:
+            self.permission_classes = [AllowAny]
+        return [permission() for permission in self.permission_classes]
+
+
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAdmin]
+        else:
+            self.permission_classes = [AllowAny]
+        return [permission() for permission in self.permission_classes]
 
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAdmin]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return [permission() for permission in self.permission_classes]
+
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.filter(
+            reservation_date=data['reservation_date']).filter(
+            reservation_slot=data['reservation_slot']).exists()
+        if exist==False:
+            booking = Booking(
+                Name=data['Name'],
+                No_of_guests=data['No_of_guests'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+    
+    date = request.GET.get('date', datetime.datetime.today().date())
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+    return HttpResponse(booking_json, content_type='application/json')
+
+def reservations(request):
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize('json', bookings)
+    return render(request, 'bookings.html',{"bookings":bookings, "bookings_json": booking_json})
+
+def menu(request):
+    menu_data = MenuItem.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
+
+
+def display_menu_item(request, pk=None): 
+    if pk: 
+        menu_item = MenuItem.objects.get(pk=pk) 
+    else: 
+        menu_item = "" 
+    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
